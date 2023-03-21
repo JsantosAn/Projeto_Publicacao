210edf7cdb508ed5b2288bfc6898ef643b9433b0
João Paulo dos Santos Andrade <jsantosan01@gmail.com>
2023-03-21 20:13:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -217,7 +217,7 @@ def qualis (Autor_Info):
             if peri[1] >= 90:
                 df_mask=qualis_periodico['Periodicos'] == str(peri[0])
                 filtered_df = qualis_periodico[df_mask]
-                i['Qualis'] = str(filtered_df.iat[0,3])
+                i['Qualis'] = str(filtered_df.iat[0,2])
                 #i['veiculo'] = str(filtered_df.iat[0,1])
                 i['inss'] = str(filtered_df.iat[0,0])
                 i['tipo_evento'] = 'Periodico'
@@ -225,7 +225,7 @@ def qualis (Autor_Info):
             if outr[1] >= 90:
                 df_mask=qualis_geral['Periodicos'] == str(outr[0])
                 filtered_df = qualis_geral[df_mask]
-                i['Qualis'] = str(filtered_df.iat[0,3])
+                i['Qualis'] = str(filtered_df.iat[0,2])
                 #i['veiculo'] = str(filtered_df.iat[0,1])
                 i['inss'] = str(filtered_df.iat[0,0])
                 i['tipo_evento'] = 'Periodico' 
@@ -234,7 +234,7 @@ def qualis (Autor_Info):
               print(conf[0])
               df_mask=qualis_conferencias['Sigla'] == str(conf[0])
               filtered_df = qualis_conferencias[df_mask]
-              i['Qualis'] = str(filtered_df.iat[0,3])
+              i['Qualis'] = str(filtered_df.iat[0,2])
               #i['veiculo'] = str(filtered_df.iat[0,3])
               i['sigla'] = str(filtered_df.iat[0,0])
               i['tipo_evento'] = 'Conferencia'
