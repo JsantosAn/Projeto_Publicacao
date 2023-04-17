e0fe26f70ad44847c4668e813d23b5d019bbba37
João Paulo dos Santos Andrade <jsantosan01@gmail.com>
2023-04-17 19:16:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -610,7 +610,7 @@ def Executa():
     csv = convert_df(tabela)
     tabela= pandas.DataFrame(tabela)
     tabela.sort_values(by='Ano', ascending=False)
-    st.dataframe(tabela.sort_values(by='Ano', ascending=False), use_container_width=True)
+    st.dataframe(tabela.sort_values(by='Ano', ascending=False).reset_index(drop=True), use_container_width=True)
     st.divider() 
     st.download_button(
         label="Download data as CSV",
